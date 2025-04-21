c616910a65783f62eb143c44c5456313f1b51485
Rohit Kulkarni <49589111+rohitkulkarni08@users.noreply.github.com>
2025-04-21 16:49:54 -0400

    Delete ingestion_pipeline directory

--- ingestion_pipeline/event_labelling.py
+++
@@ -1,8 +0,0 @@
-def add_shock_events(df):
-    df['shock_event'] = 'None'
-
-    df.loc[(df['Date'] >= '2018-02-01') & (df['Date'] <= '2018-03-31'), 'shock_event'] = 'volmageddon'
-    df.loc[(df['Date'] >= '2020-02-15') & (df['Date'] <= '2020-05-01'), 'shock_event'] = 'covid_crash'
-    df.loc[(df['Date'] >= '2022-06-01') & (df['Date'] <= '2022-12-31'), 'shock_event'] = 'fed_hike_inflation'
-
-    return df
